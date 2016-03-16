c0840998885c980e1de14ed1055b6edb48c9e34a
mohammadkabir1 <mohammad654j137@gmail.com>
2016-03-16 15:07:35 +0330

    kabirbot bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {1026818541},
+    sudo_users = 1026818541{our_id},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
